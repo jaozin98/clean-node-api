367f17c80d41eb7c45e1421057c2782a55ba90e1
João Silva <jotage.silva.dev@gmail.com>
2023-03-28 15:41:40 -0300

    refactor: move EmailValidator Creation to a factory helper method

--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface HttpResponse{
   statusCode: number
   body: any
 }
 
 export interface HttpRequest {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   body?: any
 }
