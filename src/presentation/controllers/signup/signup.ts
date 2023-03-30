3bcf68647abdd45c0d627d4873fdbc33ca4fe750
João Silva <jotage.silva.dev@gmail.com>
2023-03-30 19:33:27 -0300

    refactor:move all Signup protocols to a single file

---
+++ src/presentation/controllers/signup/signup.ts
@@ -0,0 +1,49 @@
+import { HttpResponse, HttpRequest, Controller, EmailValidator } from './signup-protocols'
+import { MissingParamError, InvalidParamError } from '../../errors'
+import { badRequest, serverError } from '../../helpers/http-helper'
+import { AddAccount } from '../../../domain/usecases/add-account'
+
+
+export class SignUpController implements Controller {
+ private readonly emailValidator:EmailValidator
+
+ private readonly addAccount:AddAccount
+
+  constructor (emailValidator:EmailValidator, addAccount: AddAccount){
+    this.emailValidator= emailValidator
+    this.addAccount = addAccount
+  }
+
+  // eslint-disable-next-line consistent-return
+  handle (httpRequest: HttpRequest): HttpResponse {
+    try {
+
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
+      // eslint-disable-next-line no-restricted-syntax
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
+      }
+      const{ name, email, password, passwordConfirmation } = httpRequest.body
+      if (password !== passwordConfirmation){
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+      const isValid = this.emailValidator.isValid(email)
+      if(!isValid){
+        return badRequest(new InvalidParamError('email'));
+      }
+      this.addAccount.add({
+        name,
+        email,
+        password
+      })
+    }catch (error){
+      return serverError()
+    }
+  }
+}
+
+
+
+
