2829342c0e4a215f8a68d68296f320862e4872f1
João Silva <jotage.silva.dev@gmail.com>
2023-04-10 19:45:51 -0300

    chore: add express

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -28,7 +28,7 @@ describe ('Bcrypt Adapter', () =>{
 
   test('Shold throw if bcrypt throw', async () => {
     const sut = makeSut()
-    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => {reject(new Error())}))
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((_resolve, reject) => {reject(new Error())}))
     const promise =  sut.encrypt('any_value')
     await expect(promise).rejects.toThrow()
   })
