c020f51cbbc5987340c5206906e6b63858600fd5
João Silva <jotage.silva.dev@gmail.com>
2023-04-11 15:23:48 -0300

    feat: connect to mongodb before app listen

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,6 +3,7 @@ import { MongoClient, Collection } from 'mongodb';
 export const MongoHelper = {
   client: null as MongoClient,
   uri: null as string,
+
   async connect(uri: string): Promise<void> {
     this.uri = uri;
     this.client = await MongoClient.connect(uri);
