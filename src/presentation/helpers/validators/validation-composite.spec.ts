e2f99815f534c8a616455c8d917d67f18c6e6134
João Silva <jotage.silva.dev@gmail.com>
2023-04-19 15:29:05 -0300

    test: ensure ValidationComposite returns an error if any validation fails

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -2,15 +2,32 @@ import { MissingParamError } from "../../errors";
 import { ValidationComposite } from "./validation-composite";
 import { Validation } from "./validation";
 
+const makeValidation  = () => {
+  class ValidationStub implements Validation {
+    validate (_input: any): Error {
+    return null
+
+    }
+  }
+    return  new ValidationStub()
+}
+interface SutTypes{
+  sut:ValidationComposite
+  validationStub: Validation
+}
+const makeSut = (): SutTypes => {
+const validationStub = makeValidation()
+const sut = new ValidationComposite([validationStub])
+return {
+  sut,
+  validationStub
+}
+}
+
 describe('Validation Composite', () => {
   test('Shold Return an error if any validations fails ', () => {
-    class ValidationStub implements Validation {
-      validate (_input: any): Error {
-      return new MissingParamError('field')
-      }
-    }
-    const validationStub = new ValidationStub()
-    const sut = new ValidationComposite([validationStub])
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
     const error = sut.validate({ field: 'any_value'})
     expect(error).toEqual(new MissingParamError('field'))
   })
