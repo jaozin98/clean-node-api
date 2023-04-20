70cf565ffd5c36939e342d29128eb71d72aae02b
João Silva <jotage.silva.dev@gmail.com>
2023-04-20 10:40:09 -0300

    refactor group validation imports in a single file

--- src/main/factories/signup/login /login-validation.ts
+++ src/main/factories/signup/login /login-validation.ts
@@ -1,15 +1,14 @@
-import { ValidationComposite } from "../../../../presentation/helpers/validators/validation-composite";
-import { RequiredFieldValidation } from "../../../../presentation/helpers/validators/required-field-validation"
+/* eslint-disable no-restricted-syntax */
+import { ValidationComposite,RequiredFieldValidation, EmailValidation } from '../../../../presentation/helpers/validators'
 import { Validation } from "../../../../presentation/protocols/validation";
 import { EmailValidatorAdapter } from "../../../../utils/emails-validator-adapter";
-import { EmailValidation } from "../../../../presentation/helpers/validators/email-validation"
 
 
 
 
 export const makeLoginValidation = (): ValidationComposite =>{
 const validations: Validation[] = []
-  // eslint-disable-next-line no-restricted-syntax
+
   for (const field of ['email', 'password']) {
     validations.push(new RequiredFieldValidation(field))
   }
--- src/main/factories/signup/login /login.validation.spec.ts
+++ src/main/factories/signup/login /login.validation.spec.ts
@@ -1,9 +1,7 @@
 /* eslint-disable no-restricted-syntax */
 import { makeLoginValidation } from "./login-validation"
-import { ValidationComposite } from "../../../../presentation/helpers/validators/validation-composite"
-import { RequiredFieldValidation } from "../../../../presentation/helpers/validators/required-field-validation"
+import { ValidationComposite, RequiredFieldValidation, EmailValidation } from "../../../../presentation/helpers/validators"
 import { Validation } from "../../../../presentation/protocols/validation"
-import { EmailValidation } from "../../../../presentation/helpers/validators/email-validation"
 import { EmailValidator } from "../../../../presentation/protocols/email-validator"
 
 
