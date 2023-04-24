279906e8fbee1c56a3424615770e664841561cc7
João Silva <jotage.silva.dev@gmail.com>
2023-04-24 12:04:53 -0300

    test: ensureDbAuthentication throws if HashComparer throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,15 +1,16 @@
+/* eslint-disable max-classes-per-file */
 import { AccountModel } from "../../../domain/models/account"
 import { AuthenticationModel } from "../../../domain/usecases/authentication"
 import { LoadAccontByEmailRepository } from "../../protocols/db/load-account-by-email-repository"
 import { DbAuthentication } from "./db-authentication"
-
+import { HashComparer } from "../../protocols/criptography/hash-comparer"
 
 const makeFakeAccount = (): AccountModel => ({
 
     id: 'any_id',
     name: 'any_name',
     email: 'any_email@mail.com',
-    password: 'any_password'
+    password: 'hashed_password'
 
 })
 
@@ -21,45 +22,70 @@ class LoadAccontByEmailRepositoryStub implements LoadAccontByEmailRepository {
 }
 return new LoadAccontByEmailRepositoryStub()
 }
+function makeHashComparer(): HashComparer {
+  class HashComparerStub implements HashComparer {
+    async compare (_value: string, _hash: string): Promise<boolean> {
+      return new Promise(resolve => resolve(true))
+    }
+  }
+  return new HashComparerStub()
+}
 
-const makeAuthentication = (): AuthenticationModel => ({
+const makeFakeAuthentication = (): AuthenticationModel => ({
   email: 'any_email@mail.com',
   password: 'any_password'
 })
 interface SutTypes {
   sut: DbAuthentication
   loadAccontByEmailRepositoryStub: LoadAccontByEmailRepository
+  hashComparerStub: HashComparer
 }
 
 const makeSut = (): SutTypes => {
 
   const loadAccontByEmailRepositoryStub = makeLoadAccontByEmailRepository()
-  const sut = new DbAuthentication(loadAccontByEmailRepositoryStub)
+  const hashComparerStub = makeHashComparer()
+  const sut = new DbAuthentication(loadAccontByEmailRepositoryStub, hashComparerStub)
   return {
     sut,
-    loadAccontByEmailRepositoryStub
+    loadAccontByEmailRepositoryStub,
+    hashComparerStub
   }
 }
 describe ('Dbauthentication UseCase', () => {
-
+  // Deve chamar LoadAccontBy Email Repository com o email correto
   test('Should call LoadAccontByEmailRepository with corret email', async () => {
     const { sut, loadAccontByEmailRepositoryStub }= makeSut()
     const loadSpy = jest.spyOn(loadAccontByEmailRepositoryStub, 'load')
-    await sut.auth(makeAuthentication())
+    await sut.auth(makeFakeAuthentication())
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
-
+  // Deve lançar se LoadAccontByEmailRepository lançar
   test('Should throw if LoadAccontByEmailRepository throws', async () => {
     const { sut, loadAccontByEmailRepositoryStub }= makeSut()
     jest.spyOn(loadAccontByEmailRepositoryStub, 'load').mockReturnValueOnce(new Promise((_resolve, reject) => reject (new Error())))
-    const promise = sut.auth(makeAuthentication())
+    const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
-
+  // Deve retornar LoadAccontByEmailRepository retorna nulo
   test('Should return LoadAccontByEmailRepository returns null', async () => {
     const { sut, loadAccontByEmailRepositoryStub }= makeSut()
     jest.spyOn(loadAccontByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
-    const accessToken = await sut.auth(makeAuthentication())
+    const accessToken = await sut.auth(makeFakeAuthentication())
     expect(accessToken).toBeNull()
+  })
+  // Deve chamar o Hash Comparer com a senha correta
+  test('Should call HashComparer with corret password', async () => {
+    const { sut, hashComparerStub } = makeSut()
+    const compareSpy = jest.spyOn(hashComparerStub, 'compare')
+    await sut.auth(makeFakeAuthentication())
+    expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
+  })
+
+  test('Should throw if HashComparer throws', async () => {
+    const { sut, hashComparerStub }= makeSut()
+    jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise((_resolve, reject) => reject (new Error())))
+    const promise = sut.auth(makeFakeAuthentication())
+    await expect(promise).rejects.toThrow()
   })
 })
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,15 +1,22 @@
 import { Authentication, AuthenticationModel } from "../../../domain/usecases/authentication";
+import { HashComparer } from "../../protocols/criptography/hash-comparer";
 import { LoadAccontByEmailRepository } from "../../protocols/db/load-account-by-email-repository";
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccontByEmailRepository
 
-  constructor (loadAccountByEmailRepository: LoadAccontByEmailRepository){
+  private readonly hashComparer: HashComparer
+
+  constructor (loadAccountByEmailRepository: LoadAccontByEmailRepository, hashComparer: HashComparer){
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
+    this.hashComparer = hashComparer
   }
 
   async auth (authentication: AuthenticationModel): Promise<string>{
-    await this.loadAccountByEmailRepository.load(authentication.email)
+    const account = await this.loadAccountByEmailRepository.load(authentication.email)
+    if (account){
+    await this.hashComparer.compare(authentication.password, account.password )
+    }
     return null
 
   }
