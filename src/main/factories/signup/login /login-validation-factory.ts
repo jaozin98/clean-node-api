96614caffb97537d544130e35f9c7269b4279504
João Gabriel <jotage.silva.dev@gmail.com>
2023-04-26 11:01:10 -0300

    refactor: move EmailValidatorAdapter to the main layer

--- src/main/factories/signup/login /login-validation-factory.ts
+++ src/main/factories/signup/login /login-validation-factory.ts
@@ -1,17 +1,14 @@
 /* eslint-disable no-restricted-syntax */
-import { ValidationComposite,RequiredFieldValidation, EmailValidation } from '../../../../presentation/helpers/validators'
-import { Validation } from "../../../../presentation/protocols/validation";
-import { EmailValidatorAdapter } from "../../../../utils/emails-validator-adapter";
+import { ValidationComposite, RequiredFieldValidation, EmailValidation } from '../../../../presentation/helpers/validators';
+import { Validation } from '../../../../presentation/protocols/validation';
+import { EmailValidatorAdapter } from '../../../adapters/validators/emails-validator-adapter';
 
-
-
-
-export const makeLoginValidation = (): ValidationComposite =>{
-const validations: Validation[] = []
+export const makeLoginValidation = (): ValidationComposite => {
+  const validations: Validation[] = [];
 
   for (const field of ['email', 'password']) {
-    validations.push(new RequiredFieldValidation(field))
+    validations.push(new RequiredFieldValidation(field));
   }
-  validations.push (new EmailValidation('email', new EmailValidatorAdapter()))
-  return new ValidationComposite(validations)
-}
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()));
+  return new ValidationComposite(validations);
+};
--- src/main/factories/signup/signup-validation-factory.ts
+++ src/main/factories/signup/signup-validation-factory.ts
@@ -1,16 +1,14 @@
-import { ValidationComposite, RequiredFieldValidation, CompareFieldsValidation, EmailValidation } from "../../../presentation/helpers/validators";
-import { Validation } from "../../../presentation/protocols/validation";
-import { EmailValidatorAdapter } from "../../../utils/emails-validator-adapter";
+import { ValidationComposite, RequiredFieldValidation, CompareFieldsValidation, EmailValidation } from '../../../presentation/helpers/validators';
+import { Validation } from '../../../presentation/protocols/validation';
+import { EmailValidatorAdapter } from '../../adapters/validators/emails-validator-adapter';
 
-
-
-export const makeSingUpValidation = (): ValidationComposite =>{
-const validations: Validation[] = []
+export const makeSingUpValidation = (): ValidationComposite => {
+  const validations: Validation[] = [];
   // eslint-disable-next-line no-restricted-syntax
-  for (const field of ['nome','email', 'password', 'passwordConfirmation']) {
-    validations.push(new RequiredFieldValidation(field))
+  for (const field of ['nome', 'email', 'password', 'passwordConfirmation']) {
+    validations.push(new RequiredFieldValidation(field));
   }
-  validations.push (new CompareFieldsValidation('password', 'passwordConfrimation;'))
-  validations.push (new EmailValidation('email', new EmailValidatorAdapter()))
-  return new ValidationComposite(validations)
-}
+  validations.push(new CompareFieldsValidation('password', 'passwordConfrimation;'));
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()));
+  return new ValidationComposite(validations);
+};
