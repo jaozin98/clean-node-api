e583dcb8a2e84a02fab9674820fb6478d27c04de
João Gabriel <jotage.silva.dev@gmail.com>
2023-04-28 22:23:08 -0300

    feat: ensure AddSurveyUseCase return 204 oin success

--- src/presentation/controllers/survey/add-survey/add-survei-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survei-controller.spec.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line max-classes-per-file
 import { HttpRequest, Validation, AddSurvey, AddSurveyModel } from './add-survey-controller-protocols';
 import { AddSurveyController } from './add-survey-controller';
-import { badRequest, serverError } from '../../../helpers/http/http-helper';
+import { badRequest, serverError, noContent } from '../../../helpers/http/http-helper';
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -79,5 +79,11 @@ describe('AddSurvey controller', () => {
     jest.spyOn(addSurveyStub, 'add').mockReturnValueOnce(new Promise((_resolve, reject) => reject(new Error())));
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  test('Shold returns 204 on success', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(noContent());
   });
 });
