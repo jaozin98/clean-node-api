fa2d0851cd9b8d96460e2f2d359769d559f45e88
João Gabriel <jotage.silva.dev@gmail.com>
2023-04-28 22:13:08 -0300

    feat: ensure AddSurveyUseCase return 500 if AddSurveyUseCase throws

--- src/presentation/controllers/survey/add-survey/add-survei-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survei-controller.spec.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line max-classes-per-file
 import { HttpRequest, Validation, AddSurvey, AddSurveyModel } from './add-survey-controller-protocols';
 import { AddSurveyController } from './add-survey-controller';
-import { badRequest } from '../../../helpers/http/http-helper';
+import { badRequest, serverError } from '../../../helpers/http/http-helper';
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -72,5 +72,12 @@ describe('AddSurvey controller', () => {
     const httpRequest = makeFakeRequest();
     await sut.handle(httpRequest);
     expect(addSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  test('Shold returns 500 if AddSurvey throws', async () => {
+    const { sut, addSurveyStub } = makeSut();
+    jest.spyOn(addSurveyStub, 'add').mockReturnValueOnce(new Promise((_resolve, reject) => reject(new Error())));
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });
--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,20 +1,24 @@
-import { badRequest } from '../../../helpers/http/http-helper';
+import { badRequest, serverError } from '../../../helpers/http/http-helper';
 import { AddSurvey, Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols';
 
 export class AddSurveyController implements Controller {
   constructor(private readonly validation: Validation, private readonly addSurvey: AddSurvey) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body);
-    if (error) {
-      return badRequest(error);
-    }
-    const { questions, answers } = httpRequest.body;
-    await this.addSurvey.add({
-      questions,
-      answers,
-    });
+    try {
+      const error = this.validation.validate(httpRequest.body);
+      if (error) {
+        return badRequest(error);
+      }
+      const { questions, answers } = httpRequest.body;
+      await this.addSurvey.add({
+        questions,
+        answers,
+      });
 
-    return null;
+      return null;
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
