0171f607f776e630c66ee0e9c8640ca054528b50
João Gabriel <jotage.silva.dev@gmail.com>
2023-05-02 20:56:10 -0300

    feat: ensure JwtAdapter call verify with correct with correct values

--- src/infra/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/jwt-adapter/jwt-adapter.spec.ts
@@ -6,6 +6,9 @@ jest.mock('jsonwebtoken', () => ({
   async sign(): Promise<string> {
     return new Promise((resolve) => resolve('any_token'));
   },
+  async verify(): Promise<string> {
+    return new Promise((_resolve) => _resolve('any_value'));
+  },
 }));
 
 const makeSut = (): JwtAdapter => new JwtAdapter('secret');
@@ -19,12 +22,14 @@ describe('jwt Adapter', () => {
       await sut.encrypt('any_id');
       expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
     });
+
     // Deve retornar um token no sucesso do sinal
     test('Should return a token on sign  success', async () => {
       const sut = makeSut();
       const accessToken = await sut.encrypt('any_id');
       expect(accessToken).toBe('any_token');
     });
+
     // Deve jogar se o sinal jogar
     test('Should throw if sign throws', async () => {
       const sut = makeSut();
@@ -33,6 +38,15 @@ describe('jwt Adapter', () => {
       });
       const promise = sut.encrypt('any_id');
       expect(promise).rejects.toThrow();
+    });
+  });
+  describe('veryfy ()', () => {
+    // Deve indicativo de chamada com valores corretos
+    test('Should call verify with correct values', async () => {
+      const sut = makeSut();
+      const verifySpy = jest.spyOn(jwt, 'verify');
+      await sut.decrypt('any_token');
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret');
     });
   });
 });
--- src/infra/jwt-adapter/jwt-adapter.ts
+++ src/infra/jwt-adapter/jwt-adapter.ts
@@ -1,8 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import jwt from 'jsonwebtoken';
 import { Encrypter } from '../../data/protocols/criptography/encrypter';
+import { Decrypter } from '../../data/protocols/criptography/decrypter';
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   private readonly secret: string;
 
   constructor(secret: string) {
@@ -12,5 +13,10 @@ export class JwtAdapter implements Encrypter {
   async encrypt(value: string): Promise<string> {
     const accessToken = await jwt.sign({ id: value }, this.secret);
     return accessToken;
+  }
+
+  async decrypt(value: string): Promise<string> {
+    await jwt.verify(value, this.secret);
+    return null;
   }
 }
