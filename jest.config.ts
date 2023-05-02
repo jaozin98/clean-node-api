0effb4f7c5850c8006a2263c16db860b2dfb2ae5
João Gabriel <jotage.silva.dev@gmail.com>
2023-05-02 10:17:29 -0300

    feat: add middleware interface

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ export default {
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
