aa329c83205399fde83a5b15cf888b9863a498fe
João Gabriel <jotage.silva.dev@gmail.com>
2023-05-09 20:34:23 -0300

    feat: ensure DbLoadSurveyById returns a SurveyResult on success

---
+++ src/data/usecases/save-survey-result/db-save-survey-result-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../protocols/db/survey/save-survey--result-repository';
+export * from '../../../domain/usecases/save-survey-result';
+export * from '../load-survey-by-id/db-load-survey-by-id-protocols';
+export * from '../../../domain/models/survey-result';
